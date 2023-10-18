eceeb900876d523eda6aeb9073ddb73d86b3eca9
Alejandro Velasquez Dev <alejandro.velzck@gmail.com>
2023-10-18 03:21:59 -0500

    Setup application
    Set up all vite react ts environment with linters & workflow

---
+++ src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import Exercises from "./components/Exercises";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Exercises />
+  </React.StrictMode>
+);
